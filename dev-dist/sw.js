41bb333fb7d298b9e21ddfa5d510ae0178e4a9c7
INDANA NITHIN <24pa1a1276@vishnu.edu.in>
2026-05-14 18:08:50 +0530

    Changed referral reward from 50 to 15 coins across Referral and Home components

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.2s3h899m3og"
+    "revision": "0.bnhvii9oem"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
